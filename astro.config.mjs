910d5bb7e51b5a041faba7f5e433a8a02dd75e7e
Sergio Majé <smajefranco@gmail.com>
2026-01-03 00:57:03 -0500

    ➕ Add astro mdx dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), mdx()]
 });
